6b2230114fbda9198527759529331dea748b9bce
Onique-Unique <kiddohbeats@yahoo.com>
2022-02-04 16:43:54 -0500

    update

--- js/gamelounge-home.js
+++ js/gamelounge-home.js
@@ -3,3 +3,5 @@ const date = new Date(Date.now());
 currentDate = date.toLocaleString("en-US", {month: "short", day: "numeric", year: "numeric"}); // remove unnecessary information from date and specify only what is needed
 
 document.getElementById("currentDate-gamelounge-home").innerHTML = currentDate;
+
+document.cookie = "AC-C=ac-c;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=None;Secure"
