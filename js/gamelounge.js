79a9c929406b074867633c4a1543549a4a6d175f
Onique-Unique <kiddohbeats@yahoo.com>
2022-02-05 17:37:12 -0500

    update

--- js/gamelounge.js
+++ js/gamelounge.js
@@ -2,6 +2,7 @@ const gameloungeBodyContainer = document.querySelector(".game-lounge-body-contai
 const formContainer = document.querySelector(".gamelounge-contact-container");
 const disclaimerContainer = document.querySelector(".fair-use-disclaimer--container");
 const downloadContainer = document.querySelector(".gamelounge-download-container");
+const searchField = document.getElementById("gamelounge-search");
 
 const emailIcon = document.getElementById("email-icon");
 const emailExit = document.querySelector(".form-exit");
@@ -12,7 +13,7 @@ const downloadIcon = document.querySelector(".download-right-arrow");
 const downloadIconSecondary = document.querySelector(".download-right-arrow-secondary");
 const downloadExit = document.getElementById("download-back");
 
-document.cookie = "AC-C=ac-c;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=None;Secure"
+document.cookie = "AC-C=ac-c;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=None;Secure";
 
 // Index Banner Date Logic 
 // Get Current Date JS Logic
@@ -27,12 +28,37 @@ for(i = 0; i < dateMain.length; i++){
         document.getElementById(dateMain[i]).innerHTML = "This Week " + currentDate;
     }else if (i === 1){
         document.getElementById(dateMain[i]).innerHTML = "Today " + currentDate;
-    // }else if(i === 2){
+    }// else if(i === 2){
     //     document.getElementById(dateMain[i]).innerHTML = currentDate + " " + dot + " Popular"; - Top Videos Section
-    } else if (i > 2){
-        document.getElementById(dateMain[i]).innerHTML = currentDate + " " + dot + " News";
-    }
-    
+    // } else if (i > 2){
+    //     document.getElementById(dateMain[i]).innerHTML = currentDate + " " + dot + " News";
+    // }
+};
+
+
+// writing a script for the search bar function logic
+// for the explore page will identify different sections searched using location.href = "#idName"
+function openfile() {
+    // Game Lounge Search Bar Script
+    // a script allowing search user to find an option between multiple strings/ words that may lead to the same content
+    String.prototype.includesOneof = function(arrayOfStrings) {
+
+        if (!Array.isArray(arrayOfStrings)) {
+            throw new Error("includesOneOf only accepts an array");
+        }
+        return arrayOfStrings.some(str => this.includes(str));
+    };
+
+    var info = document.getElementById("gamelounge-search").value;
+    var action1 = "https://www.youtube.com/results";
+    var action2 = "https://www.youtube.com/results";
+
+    if (!info.toLowerCase().includesOneof(["apex", "legends", "alm"])) {
+        document.getElementById("gamelounge-search").value = info + " apex legends mobile";
+        document.getElementById("form-yt--search").action = action1; 
+    }else {
+        document.getElementById("form-yt--search").action = action2;
+    };
 };
 
 emailIcon.addEventListener("click", () => {
@@ -74,4 +100,4 @@ downloadIconSecondary.addEventListener("click", () => {
 downloadExit.addEventListener("click", () => {
     gameloungeBodyContainer.classList.toggle("hide");
     downloadContainer.classList.toggle("hide");
-})
+});
