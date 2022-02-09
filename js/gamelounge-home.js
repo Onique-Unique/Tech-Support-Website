794584c711576d786ced056b3a99410fa3accae3
Onique-Unique <kiddohbeats@yahoo.com>
2022-02-09 06:26:40 -0500

    link update

--- js/gamelounge-home.js
+++ js/gamelounge-home.js
@@ -10,5 +10,4 @@ document.cookie = "AC-C=ac-c;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSi
 
 homeBtn.addEventListener("click", () => {
     alert("You Have Been Invited To Participate In A Server Stress Test - Follow Details Mentioned In Discord And Report Feedback To The Community Developer. Click Ok To Proceed...");
-    window.location.href="`/main-pages/test-apex-mobile/game-lounge-chatroom`";
 });
