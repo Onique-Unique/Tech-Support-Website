1ae35592bdefe7159fd25cfa97afde91f2490214
Onique-Unique <kiddohbeats@yahoo.com>
2022-02-24 15:59:31 -0500

    Soft Launch Changes

--- js/gamelounge.js
+++ js/gamelounge.js
@@ -58,7 +58,7 @@ for(i = 0; i < dateMain.length; i++){
 };
 
 // Playlist Switch Random
-const playlistArrayId = ["-9gEgshJUuY", "ok_-2KkKQF4", "3sp0wd8j8CQ", "aL14iT2ix0w", "7tNtU5XFwrU", "8NzYo0jmYek", "mZHTwjLznVg", "-6v0vFVp52w", "3bp1kIUlYEM", "wKwR2CRbVVo"];
+const playlistArrayId = ["-9gEgshJUuY", "ok_-2KkKQF4", "3sp0wd8j8CQ", "aL14iT2ix0w", "7tNtU5XFwrU", "8NzYo0jmYek", "mZHTwjLznVg", "-6v0vFVp52w", "FQRtxvUc8W4", "3bp1kIUlYEM", "wKwR2CRbVVo", "6Irus3d5f0E", "7NOSDKb0HlU", "tCs48OFv7xA", "GDQnA1LVCWA", "rXsZk4Mz_Ac"];
 const playlistRefresh = document.querySelector(".playlist-random");
 
 playlistRefresh.addEventListener("click", () => {  
