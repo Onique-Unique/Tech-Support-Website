ec79aad212bcacb7008987cd6ecc4c2ebb60755e
Onique-Unique <kiddohbeats@yahoo.com>
2022-03-27 16:41:52 -0400

    Content Update

--- js/archive-modal.js
+++ js/archive-modal.js
@@ -8,12 +8,12 @@ document.getElementById("content-modal").innerHTML = `<i class="fa-solid fa-chev
     </div>
     <div id="other-news-listings">
         <h3>Soft Launch Notes!</h3>
-        <a href="/main-pages/apex-mobile/best-legends-soft-launch"><h2>Apex Legends Mobile Top 6 Legends to Use Right Now!</h2></a>
-        <a href="/main-pages/apex-mobile/apex-mobile-cargo-bot"><h2>Apex Legends Mobile Cargo Bot Complete Guide</h2></a>
-        <a href="/main-pages/apex-mobile/best-guns-soft-launch"><h2>Apex Legends Mobile Best Guns - Soft Launch</h2></a>
+        <a href="/main-pages/apex-mobile/soft-launch-notes/best-legends-soft-launch"><h2>Apex Legends Mobile Top 6 Legends to Use Right Now!</h2></a>
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-mobile-cargo-bot"><h2>Apex Legends Mobile Cargo Bot Complete Guide</h2></a>
+        <a href="/main-pages/apex-mobile/soft-launch-notes/best-guns-soft-launch"><h2>Apex Legends Mobile Best Guns - Soft Launch</h2></a>
         <a href="/main-pages/apex-mobile/game-lounge-chatroom#exclusive-content"><h2>Apex Legends Mobile Exclusive Content</h2></a>
         <a href="/main-pages/apex-mobile/game-lounge-chatroom#season-1"><h2>Apex Legends Mobile Season 1 Arrivals</h2></a>
-        <a href="/main-pages/apex-mobile/how-to-wall-jump"><h2>Apex Legends Mobile How to Wall Jump/ Wall Bounce</h2></a>
-        <a href="/main-pages/apex-mobile/apex-mobile-ranked"><h2>Apex Legends Mobile Ranked System Full Explained</h2></a>
+        <a href="/main-pages/apex-mobile/soft-launch-notes/how-to-wall-jump"><h2>Apex Legends Mobile How to Wall Jump/ Wall Bounce</h2></a>
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-mobile-ranked"><h2>Apex Legends Mobile Ranked System Full Explained</h2></a>
     </div>
 </div>`
