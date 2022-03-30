13118c15d283f51c5c7044e2e075b0b4695224a4
Onique-Unique <kiddohbeats@yahoo.com>
2022-03-30 14:31:10 -0400

    Content Update

---
+++ js/soft-launch-notes.js
@@ -0,0 +1,84 @@
+document.getElementById("soft-launch-crumbs").innerHTML = 
+`
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-legends-characters-coming-to-mobile"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/qYpj-6dpSRU/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Characters - coming to Mobile</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-legends-wraith-infinite-void"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/JG-9HeDBXzA/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile - Wraith Infinite Void Glitch</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-legends-mobile-fov-settings"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/zKGP6I6t74o/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile FOV Settings Guide for Beginners</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-legends-mobile-release-date"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/QdwbDFXBDVo/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Release Date</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/best-legends-soft-launch"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/CUqGO9VUB3c/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Top 6 Legends to use Right Now!</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-mobile-cargo-bot"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/fghYZb2spO4/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Cargo Bot Complete Guide</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/best-guns-soft-launch"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/qmOfJwyISUk/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Best Guns - Soft Launch</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/worst-guns-soft-launch"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/AqL1VQI49tk/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Worst Guns - Soft Launch</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-mobile-ranked"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/GanP2hz39To/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Ranked System Full Explained</h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/soft-launch-notes/how-to-wall-jump"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/keBVfPVUzaU/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile - How to Wall Jump/ Wall Bounce</h2>
+    </div>
+    <hr class="crumb-hr">
+</div> 
+<!--  ------------------------------------------------------------------------------  -->
+
+`
