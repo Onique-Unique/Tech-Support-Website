ee60ed1babff9005402e0ebb1345f9820305efd8
Onique-Unique <kiddohbeats@yahoo.com>
2022-03-30 17:35:17 -0400

    Content Update

--- js/soft-launch-notes.js
+++ js/soft-launch-notes.js
@@ -10,7 +10,7 @@ document.getElementById("soft-launch-crumbs").innerHTML =
 <!--  ------------------------------------------------------------------------------  -->
 <div class="articles-main-crumbs--inner">
     <div class="main-crumbs-contain">
-        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-legends-wraith-infinite-void"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/JG-9HeDBXzA/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <a href="/main-pages/apex-mobile/soft-launch-notes/wraith-infinite-void"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/JG-9HeDBXzA/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
         <h2 class="article-crumb-h2">Apex Legends Mobile - Wraith Infinite Void Glitch</h2>
     </div>
     <hr class="crumb-hr">
