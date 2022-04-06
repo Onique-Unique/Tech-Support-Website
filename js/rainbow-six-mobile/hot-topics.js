ce48bc566205b14ee185e8f5894f8b91332870b6
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-06 11:43:21 -0400

    Content Update

--- js/rainbow-six-mobile/hot-topics.js
+++ js/rainbow-six-mobile/hot-topics.js
@@ -2,13 +2,15 @@ const topics = ["topic-PC", "topic-Mobile"];
 
 for(let i = 0; i < topics.length; i++){
     document.getElementById(topics[i]).innerHTML = `
-    <h2>New Banner For Rainbow Six Mobile Launch...</h2><br>
-    <img loading="lazy" src="/images/game-lounge-img/r6-hot-topics-assets/apexmobile-new-reward.PNG" alt="rainbow six mobile gamelounge Hot topics image">                   
-    <p>Rainbow Six Mobile Free Reward for Pre registered accounts before launch day. Android Devices are open for pre registration, for a chance to earn exclusive rewards.
+    <h2>Rainbow Six Mobile Alpha Test Launch...</h2><br>
+    <img loading="lazy" src="/images/game-lounge-img/r6-hot-topics-assets/r6-announcement-closed-alpha.PNG" alt="rainbow six mobile gamelounge Hot topics image">                   
+    <p>Alpha Test Launch for Rainbow Six Mobile starting in spring.
     </p> <br>
     <div id="topic-space-br"></div>
-    <h2>What's The hype - Reddit..</h2>
-    <p> Reddit sources have included Rainbow Six Mobile as one of the hottest upcoming games to be featured on mobile devices.
-        Rainbow Six Mobile is deemed as one of Respawns most exciting upcoming releases, the enthusiasm is high.
-    </p>`
+    <h2>Rainbow Six Mobile Pre release Sign Up...</h2><br>
+    <img loading="lazy" src="/images/game-lounge-img/r6-hot-topics-assets/r6-announcement-signup.PNG" alt="rainbow six mobile gamelounge Hot topics image">                   
+    <p>Rainbow Six Mobile announces Pre release signup for alpha test stage.
+    </p> <br>
+    <div id="topic-space-br"></div>   
+    `
 };
