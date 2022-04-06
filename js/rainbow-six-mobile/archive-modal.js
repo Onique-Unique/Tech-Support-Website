0db64d386e68f94adff88b22b8d8be3cb1a73e3a
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-06 17:42:22 -0400

    Content Update

--- js/rainbow-six-mobile/archive-modal.js
+++ js/rainbow-six-mobile/archive-modal.js
@@ -4,10 +4,10 @@ document.getElementById("content-modal").innerHTML = `<i class="fa-solid fa-chev
     <h2 id="h2-secondary">NEWS & UPDATES</h2>
     <div id="featured-news-listing">
         <h3>featured</h3>
-        <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#aim-assist"><h2 id="featured-listing">Rainbow Six Mobile Aim Assist</h2></a>
+        <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#top-article"><h2 id="featured-listing">Rainbow Six Mobile Spring Launch</h2></a>
     </div>
     <div id="other-news-listings">
         <h3>Soft Launch Notes!</h3>
-        <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-launch"><h2>Rainbow Six Mobile Launch</h2></a>
+        <a href="/main-pages//rainbow-six-mobile/game-lounge-chatroom#exclusive-content"><h2>Rainbow Six Mobile Exclusive Content</h2></a>
     </div>
 </div>`
