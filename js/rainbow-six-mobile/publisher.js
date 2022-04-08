ab59bdf4bbdb36e0b519b1eb6580f142d6d33eb2
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-08 06:03:34 -0400

    R6 Mobile Publisher Update

--- js/rainbow-six-mobile/publisher.js
+++ js/rainbow-six-mobile/publisher.js
@@ -8,7 +8,7 @@ document.getElementById("publishments").innerHTML = `<div class="publishments">
     <div class="flex-publisher">
         <div id="left-section">
             <div class="verified-publisher">
-                <h2>1ceStream</h2>
+                <h2>Featured</h2>
                 <svg width="35" height="25" viewBox="0 0 327 313" fill="rgb(72, 255, 0)" xmlns="http://www.w3.org/2000/svg">
                     <g id="checkmark">
                     <path id="outer" d="M51.0938 156.5C51.0907 137.612 56.2823 119.056 66.1467 102.697C76.0112 86.3386 90.2008 72.7538 107.289 63.3085C124.378 53.8631 143.763 48.8902 163.496 48.8894C183.229 48.8887 202.614 53.8602 219.703 63.3043C222.049 64.5809 224.827 64.9175 227.431 64.2408C230.035 63.564 232.254 61.9288 233.603 59.692C234.953 57.4551 235.323 54.7983 234.633 52.3011C233.943 49.804 232.25 47.6694 229.922 46.3631C204.597 32.3678 175.155 26.7619 146.163 30.415C117.17 34.0681 90.2467 46.7759 69.5683 66.5677C48.8899 86.3595 35.6119 112.129 31.7936 139.88C27.9752 167.631 33.8299 195.813 48.4497 220.055C63.0694 244.296 85.6372 263.243 112.653 273.957C139.669 284.671 169.623 286.552 197.87 279.31C226.117 272.068 251.079 256.107 268.883 233.903C286.687 211.698 296.34 184.491 296.344 156.5C296.344 153.906 295.267 151.418 293.351 149.584C291.434 147.749 288.835 146.719 286.125 146.719C283.415 146.719 280.816 147.749 278.899 149.584C276.983 151.418 275.906 153.906 275.906 156.5C275.906 185.036 264.064 212.403 242.983 232.58C221.903 252.758 193.312 264.094 163.5 264.094C133.688 264.094 105.097 252.758 84.0168 232.58C62.9365 212.403 51.0938 185.036 51.0938 156.5Z" fill="black"/>
@@ -17,25 +17,25 @@ document.getElementById("publishments").innerHTML = `<div class="publishments">
                 </svg>
             </div> 
             <div class="about-publisher">
-                <p>UK Based Content Creator and Streamer for <span>Apex Legends Mobile,</span> Bringing you the best Guides, Gameplay and News;
-                    Unrivalled energy, loud and unique - Proud member of Wicked eSports
+                <p>Are you a Rainbow six (6) content publisher/ run a website? Make guides?</span> - Get Featured on the GameLounge Now!!!
+                    No pre requisites or sign ups required.
                 </p>
-                <p><span>1ceStream, you scream</span></p>
-                <a href="https://www.youtube.com/c/1ceStream" target="_blank"><button id="channel-btn">Visit Channel</button></a>
+                <p><span>Become a featured publisher</span></p>
+                <a href="#" target="_blank"><button id="channel-btn">template</button></a>
             </div>                      
         </div>
         <div id="right-section">
-            <img loading="lazy" src="/images/game-lounge-img/content-creators/New_1ceStream_Logo_by_Acuity_-_Black_Outline_White_BG_Square-removebg-preview.png" alt=" 1ceStream.com 1ceStream Logo">
+            <img loading="lazy" src="/images/game-lounge-img/Your-Logo-here.PNG" alt=" Logo Here">
         </div>
     </div>   
     <div class="legend-carousel">
-        <h2>Legends Guide</h2>
+        <h2>Operators Guide</h2>
         <div id="legend-inner-header">
             <h3>Soft Launch</h3>
             <p>Pre Release Notes - v1.0.0</p>
         </div>
         <div class="legend-row">
-            <a href="https://www.1cestream.com/bangalore" target="_blank" rel="nofollow"><img loading="lazy" title="Bangalore" src="/images/game-lounge-img/1cestream-assets/legends/bangalore.png" alt="Apex Legends Mobile Gamelounge"></a>
+        <!-- <a href="https://www.1cestream.com/bangalore" target="_blank" rel="nofollow"><img loading="lazy" title="Bangalore" src="/images/game-lounge-img/1cestream-assets/legends/bangalore.png" alt="Apex Legends Mobile Gamelounge"></a>
             <a href="https://www.1cestream.com/bloodhound" target="_blank" rel="nofollow"><img loading="lazy" title="Bloodhound" src="/images/game-lounge-img/1cestream-assets/legends/bloodhound.png" alt="Apex Legends Mobile Gamelounge"></a>
             <a href="https://www.1cestream.com/caustic" target="_blank" rel="nofollow"><img loading="lazy" title="Caustic" src="/images/game-lounge-img/1cestream-assets/legends/caustic.png" alt="Apex Legends Mobile Gamelounge"></a>
             <a href="https://www.1cestream.com/crypto" target="_blank" rel="nofollow"><img loading="lazy" title="Crypto" src="/images/game-lounge-img/1cestream-assets/legends/crypto.png" alt="Apex Legends Mobile Gamelounge"></a>
@@ -45,7 +45,8 @@ document.getElementById("publishments").innerHTML = `<div class="publishments">
             <a href="https://www.1cestream.com/pathfinder" target="_blank" rel="nofollow"><img loading="lazy" title="Pathfinder" src="/images/game-lounge-img/1cestream-assets/legends/pathfinder.png" alt="Apex Legends Mobile Gamelounge"></a>
             <a href="https://www.1cestream.com/revenant" target="_blank" rel="nofollow"><img loading="lazy" title="Revenant" src="/images/game-lounge-img/1cestream-assets/legends/revenant.png" alt="Apex Legends Mobile Gamelounge"></a>
             <a href="https://www.1cestream.com/wattson" target="_blank" rel="nofollow"><img loading="lazy" title="Wattson" src="/images/game-lounge-img/1cestream-assets/legends/wattson.png" alt="Apex Legends Mobile Gamelounge"></a>
-            <a href="https://www.1cestream.com/wraith" target="_blank" rel="nofollow"><img loading="lazy" title="Wraith" src="/images/game-lounge-img/1cestream-assets/legends/wraith.png" alt="Apex Legends Mobile Gamelounge"></a>
+            <a href="https://www.1cestream.com/wraith" target="_blank" rel="nofollow"><img loading="lazy" title="Wraith" src="/images/game-lounge-img/1cestream-assets/legends/wraith.png" alt="Apex Legends Mobile Gamelounge"></a> 
+        -->  
         </div> 
 
         <!-- Custom Ad spot -->
@@ -77,7 +78,7 @@ document.getElementById("publishments").innerHTML = `<div class="publishments">
             <p>Pre Release Notes - v1.0.0</p>
         </div>
         <div class="legend-row">
-            <!-- <a href="https://www.1cestream.com/bangalore" target="_blank" rel="nofollow"><img loading="lazy" title="Bangalore" src="/images/game-lounge-img/1cestream-assets/legends/bangalore.png" alt="Apex Legends Mobile"></a>
+        <!-- <a href="https://www.1cestream.com/bangalore" target="_blank" rel="nofollow"><img loading="lazy" title="Bangalore" src="/images/game-lounge-img/1cestream-assets/legends/bangalore.png" alt="Apex Legends Mobile"></a>
             <a href="https://www.1cestream.com/bloodhound" target="_blank" rel="nofollow"><img loading="lazy" title="Bloodhound" src="/images/game-lounge-img/1cestream-assets/legends/bloodhound.png" alt="Apex Legends Mobile"></a>
             <a href="https://www.1cestream.com/caustic" target="_blank" rel="nofollow"><img loading="lazy" title="Caustic" src="/images/game-lounge-img/1cestream-assets/legends/caustic.png" alt="Apex Legends Mobile"></a>
             <a href="https://www.1cestream.com/crypto" target="_blank" rel="nofollow"><img loading="lazy" title="Crypto" src="/images/game-lounge-img/1cestream-assets/legends/crypto.png" alt="Apex Legends Mobile"></a>
@@ -87,7 +88,8 @@ document.getElementById("publishments").innerHTML = `<div class="publishments">
             <a href="https://www.1cestream.com/pathfinder" target="_blank" rel="nofollow"><img loading="lazy" title="Pathfinder" src="/images/game-lounge-img/1cestream-assets/legends/pathfinder.png" alt="Apex Legends Mobile"></a>
             <a href="https://www.1cestream.com/revenant" target="_blank" rel="nofollow"><img loading="lazy" title="Revenant" src="/images/game-lounge-img/1cestream-assets/legends/revenant.png" alt="Apex Legends Mobile"></a>
             <a href="https://www.1cestream.com/wattson" target="_blank" rel="nofollow"><img loading="lazy" title="Wattson" src="/images/game-lounge-img/1cestream-assets/legends/wattson.png" alt="Apex Legends Mobile"></a>
-            <a href="https://www.1cestream.com/wraith" target="_blank" rel="nofollow"><img loading="lazy" title="Wraith" src="/images/game-lounge-img/1cestream-assets/legends/wraith.png" alt="Apex Legends Mobile"></a> -->
+            <a href="https://www.1cestream.com/wraith" target="_blank" rel="nofollow"><img loading="lazy" title="Wraith" src="/images/game-lounge-img/1cestream-assets/legends/wraith.png" alt="Apex Legends Mobile"></a> 
+        -->
         </div> 
         
          <!-- Custom Ad spot -->                    
