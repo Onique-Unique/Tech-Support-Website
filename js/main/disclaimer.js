af031d5e83ac3cbe9c038a168a8982d9ebc39d76
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-11 14:25:18 -0400

    Content Update

--- js/main/disclaimer.js
+++ js/main/disclaimer.js
@@ -7,7 +7,7 @@
 // document.getElementsByTagName("head")[-1].appendChild(meta);
 
 // Main Code -----------
-document.head.innerHTML += `<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com; script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com ">`
+// document.head.innerHTML += `<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com; script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com ">`
 
 // Feenixbloom Privacy Policy - Terms & Conditions - Disclaimers --------------------------
 
