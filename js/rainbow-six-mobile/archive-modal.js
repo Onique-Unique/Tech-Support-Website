9afc4cadc53258520b6512a3160f813b313ebc8d
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-13 23:13:16 -0400

    Content Update

--- js/rainbow-six-mobile/archive-modal.js
+++ js/rainbow-six-mobile/archive-modal.js
@@ -4,11 +4,12 @@ document.getElementById("content-modal").innerHTML = `<i class="fa-solid fa-chev
     <h2 id="h2-secondary">NEWS & UPDATES</h2>
     <div id="featured-news-listing">
         <h3>featured</h3>
-        <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#top-article"><h2 id="featured-listing">Rainbow Six Mobile Spring Launch</h2></a>
+        <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#top-article"><h2 id="featured-listing">Rainbow Six (6) Mobile Spring Launch</h2></a>
     </div>
     <div id="other-news-listings">
         <h3>Soft Launch Notes!</h3>
-        <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#exclusive-content"><h2>Rainbow Six Mobile Exclusive Content</h2></a>
+        <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-operators"><h2>Rainbow Six (6) Mobile Upcoming Operators - Alpha Test</h2></a>
+        <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#exclusive-content"><h2>Rainbow Six (6) Mobile Exclusive Content</h2></a>
         <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-release-date"><h2>Rainbow Six (6) Mobile Release Date</h2></a>
     </div>
 </div>`
