3a5ca5765654b561be998490c96066b9d222eb77
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-14 01:09:20 -0400

    Content Update

--- js/rainbow-six-mobile/soft-launch-notes.js
+++ js/rainbow-six-mobile/soft-launch-notes.js
@@ -2,7 +2,7 @@ document.getElementById("soft-launch-crumbs").innerHTML =
 `
 <div class="articles-main-crumbs--inner">
     <div class="main-crumbs-contain">
-        <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-release-date"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/qK_Got7F7l4/hqdefault.jpg" alt="rainbow six mobile gamelounge thumbnail" frameborder="0"></a>
+        <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-operators"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/qK_Got7F7l4/hqdefault.jpg" alt="rainbow six mobile gamelounge thumbnail" frameborder="0"></a>
         <h2 class="article-crumb-h2">Rainbow Six (6) Mobile Upcoming Operators</h2>
     </div>
     <hr class="crumb-hr">
