bc76bf8b2fd3c0b77cde5cd08719e6d826ead9d2
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-25 20:23:17 -0400

    Ad check test

--- js/main/disclaimer.js
+++ js/main/disclaimer.js
@@ -9,6 +9,15 @@
 // Main Code -----------
 // document.head.innerHTML += `<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com; script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com ">`
 
+// AD Check----
+var scriptTag = document.createElement('script'); 
+scriptTag.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7079047961613379"
+crossorigin="anonymous"; // set the src attribute
+// scriptTag.type = 'text/javascript'; // if you have an HTML5 website you may want to comment this line out
+scriptTag.async = true; // the HTML5 async attribute
+var blockTag = document.getElementsByTagName('blockquote')[0];
+blockTag.appendChild(scriptTag);
+
 //Important Consent Modal Insert JS ---------------------------------------------------------
 const bodyInsert = document.querySelector(".game-lounge-body");
 let consentInfo = document.createElement("div");
