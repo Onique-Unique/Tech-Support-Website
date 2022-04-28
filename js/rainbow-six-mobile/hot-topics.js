d7e268fb8e5a7cf7bb170e84338863e7cb9df458
Onique-Unique <kiddohbeats@yahoo.com>
2022-04-28 14:32:16 -0400

    Content Update

--- js/rainbow-six-mobile/hot-topics.js
+++ js/rainbow-six-mobile/hot-topics.js
@@ -2,25 +2,16 @@ const topics = ["topic-PC", "topic-Mobile"];
 
 for(let i = 0; i < topics.length; i++){
     document.getElementById(topics[i]).innerHTML = `
-    <h2>Rainbow Six (6) Mobile Alpha Test Release Announcement...</h2><br>
-    <img loading="lazy" src="/images/game-lounge-img/r6-hot-topics-assets/r6-announcement-closed-alpha-release.PNG" alt="rainbow six mobile gamelounge Hot topics image">                   
-    <p>The Alpha test for Rainbow Six (6) Mobile has been officially announced to launch - May 3rd 2022.
-    </p> <br>
-    <div class="topic-space-br"></div>
-    <h2>Rainbow Six (6) Mobile Community Developer Team...</h2><br>
-    <img loading="lazy" src="/images/game-lounge-img/r6-hot-topics-assets/r6-community-team.PNG" alt="rainbow six mobile gamelounge Hot topics image">                   
-    <p>Hype and Positive Community Reactions since the official announcement of Rainbow six (6) coming to mobile.
-    </p> <br>
-    <div class="topic-space-br"></div>
-    <h2>Rainbow Six (6) Mobile Alpha Test Launch...</h2><br>
-    <img loading="lazy" src="/images/game-lounge-img/r6-hot-topics-assets/r6-announcement-closed-alpha.PNG" alt="rainbow six mobile gamelounge Hot topics image">                   
-    <p>Alpha Test Launch for Rainbow Six (6) Mobile starting in spring.
-    </p> <br>
-    <div class="topic-space-br"></div>
-    <h2>Rainbow Six (6) Mobile Pre release Sign Up...</h2><br>
-    <img loading="lazy" src="/images/game-lounge-img/r6-hot-topics-assets/r6-announcement-signup.PNG" alt="rainbow six mobile gamelounge Hot topics image">                   
-    <p>Rainbow Six (6) Mobile announces Pre release signup for alpha test stage.
-    </p> <br>
-    <div class="topic-space-br"></div>   
+    <a class="twitter-timeline" href="https://twitter.com/Rainbow6Mobile?ref_src=twsrc%5Etfw">Tweets by Rainbow6Mobile</a>  
     `
 };
+
+window.addEventListener("load", () => {
+    const bodyInsert = document.querySelector(".game-lounge-body");
+    var apexTweetScriptTag = document.createElement('script'); 
+    apexTweetScriptTag.src = "https://platform.twitter.com/widgets.js"
+    charset="utf-8"; // set the src attribute
+    // scriptTag.type = 'text/javascript'; // if you have an HTML5 website you may want to comment this line out
+    apexTweetScriptTag.async = true; // the HTML5 async attribute
+    bodyInsert.appendChild(apexTweetScriptTag);
+});
