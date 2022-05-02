97a5b40aadb905f92f64199ad0c06bfb7ae0ca49
Onique-Unique <kiddohbeats@yahoo.com>
2022-05-02 19:34:46 -0400

    MailChimp ad

--- js/main/ads-misc-insert.js
+++ js/main/ads-misc-insert.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", () => {
     // Discord Chat Insert Top ------------------------------
     const bodyInsert = document.querySelector(".game-lounge-body");   
+    const headInsert = document.getElementsByTagName("head")[0];
     var discordScriptTag = document.createElement('script');
     discordScriptTag.src = "https://cdn.jsdelivr.net/npm/@widgetbot/html-embed";
     discordScriptTag.async = true;
@@ -26,4 +27,12 @@ window.addEventListener("load", () => {
     adScriptTag.async = true; // the HTML5 async attribute
     var blockTag = document.getElementsByTagName('blockquote')[0];
     blockTag.appendChild(adScriptTag);
+
+    //Mail Chimp Subscription-----------------------------
+    var mailChimpSubTag = document.createElement('script');
+    mailChimpSubTag.id = "mailchimp-sub";
+    mailChimpSubTag.innerHTML = `
+        id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/2cd40847db1a4a5ab0a438183/dfc1926a8e7593b479f4fbcb6.js");
+    `
+    headInsert.appendChild(mailChimpSubTag)
 });
