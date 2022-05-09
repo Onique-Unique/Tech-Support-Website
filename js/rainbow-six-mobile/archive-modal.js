37c5436572a170fbc00b5b8de22d48d4e220d8a6
Onique-Unique <kiddohbeats@yahoo.com>
2022-05-09 12:24:02 -0400

    Content Update

--- js/rainbow-six-mobile/archive-modal.js
+++ js/rainbow-six-mobile/archive-modal.js
@@ -2,12 +2,20 @@ document.getElementById("content-modal").innerHTML = `<i class="fa-solid fa-chev
 <div class="modal-content">
     <h1>RAINBOW SIX MOBILE</h1>
     <h2 id="h2-secondary">NEWS & UPDATES</h2>
+    <div class="all-notes-scroll">
+        <div class="notes-title">
+            <a href="#soft-launch--notes" class="notes-link notes-link--active">Soft Launch Notes!</a>
+        </div>
+        <!-- <div class="notes-title">
+            <a href="#" class="notes-link">coming soon title</a>
+        </div> -->
+    </div>
     <div id="featured-news-listing">
         <h3>featured</h3>
         <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#top-article"><h2 id="featured-listing">Rainbow Six (6) Mobile Spring Launch</h2></a>
     </div>
-    <div id="other-news-listings">
-        <h3>Soft Launch Notes!</h3>
+    <div class="other-news-listings">
+        <h3 id="soft-launch--notes">Soft Launch Notes!</h3>
         <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-crossplay-controller-support"><h2>Rainbow Six (6) to feature Crossplay & Controller Support for Mobile</h2></a>
         <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-update-alpha-test"><h2>Rainbow Six (6) Update Patch Notes Mobile - Alpha Test</h2></a>
         <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-esports"><h2>4 Facts about - Rainbow Six (6) Mobile Esports Scene</h2></a>
