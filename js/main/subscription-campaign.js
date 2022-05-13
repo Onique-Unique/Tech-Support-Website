c5c9356ec85822719b51da76cfc36bb0ef74dde9
Onique-Unique <kiddohbeats@yahoo.com>
2022-05-12 20:18:04 -0400

    Content Update

--- js/main/subscription-campaign.js
+++ js/main/subscription-campaign.js
@@ -155,11 +155,37 @@ window.addEventListener("load", () => {
     });
 
     // Get Current Date JS Logic----------------------------------------------------
-    const date = new Date(Date.now());
-    currentDate = date.toLocaleString("en-US", {month: "short", day: "numeric", year: "numeric"}); // remove unnecessary information from date and specify only what is needed
 
+    // Set the date we're counting down to
+    var countDownDate = new Date("Jul 31, 2022 15:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Dot symbol as separation for text limited time only
     const dot = " &bull; ";
 
-    document.getElementById("subscription-time").innerHTML = currentDate + dot + " limited time only";
+    // Display the result in the element with id="demo"
+    document.getElementById("subscription-time").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s " + dot + " limited time only";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("subscription-time").innerHTML = "EXPIRED";
+        }
+    }, 1000);
 });
 
