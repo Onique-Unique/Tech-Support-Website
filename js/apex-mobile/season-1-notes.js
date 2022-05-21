7b2c577bea67d6faa73e6f9299c6482b8ee40eaa
Onique-Unique <kiddohbeats@yahoo.com>
2022-05-21 13:07:07 -0400

    Content Update

--- js/apex-mobile/season-1-notes.js
+++ js/apex-mobile/season-1-notes.js
@@ -2,8 +2,8 @@ document.getElementById("season-1-crumbs").innerHTML =
 `
 <div class="articles-main-crumbs--inner">
     <div class="main-crumbs-contain">
-    <a href="/main-pages/apex-mobile/soft-launch-notes/apex-legends-mobile-solo-mode"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/25L0v1xvvnw/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
-        <h2 class="article-crumb-h2">Apex Legends Mobile How to play Solo Mode/ Solo Queue?</h2>
+        <a href="/main-pages/apex-mobile/soft-launch-notes/apex-mobile-ranked"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/3gvQRIrTcjw/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Season One (1) Ranked System </h2>
     </div>
     <hr class="crumb-hr">
 </div>
