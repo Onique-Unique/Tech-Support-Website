6d732c07b59e1ed4657a43ec23e5807829a8a0ee
Onique-Unique <kiddohbeats@yahoo.com>
2022-05-21 21:01:18 -0400

    Content Update

--- js/apex-mobile/season-1-notes.js
+++ js/apex-mobile/season-1-notes.js
@@ -2,6 +2,14 @@ document.getElementById("season-1-crumbs").innerHTML =
 `
 <div class="articles-main-crumbs--inner">
     <div class="main-crumbs-contain">
+        <a href="/main-pages/apex-mobile/notes-season-list/season-1-notes/apex-legends-mobile-gyroscope-settings"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/wDP5QkgNqQ8/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
+        <h2 class="article-crumb-h2">Apex Legends Mobile Best Gyroscope Sensitivity Settings </h2>
+    </div>
+    <hr class="crumb-hr">
+</div>
+<!--  ------------------------------------------------------------------------------  -->
+<div class="articles-main-crumbs--inner">
+    <div class="main-crumbs-contain">
         <a href="/main-pages/apex-mobile/soft-launch-notes/apex-mobile-ranked"><img loading="lazy" class="article-crumb-direction" src="https://i3.ytimg.com/vi/3gvQRIrTcjw/hqdefault.jpg" alt="apex legends mobile gamelounge thumbnail" frameborder="0"></a>
         <h2 class="article-crumb-h2">Apex Legends Mobile Season One (1) Ranked System </h2>
     </div>
