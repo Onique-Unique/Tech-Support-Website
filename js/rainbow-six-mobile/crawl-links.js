a9238a409dc41e452918f37074024c01d29468ed
Onique-Unique <kiddohbeats@yahoo.com>
2022-05-25 06:41:36 -0400

    Site Crawl Links Update

---
+++ js/rainbow-six-mobile/crawl-links.js
@@ -0,0 +1,19 @@
+document.getElementById("crawl-links").innerHTML = 
+`
+    <!-- Top Featured Breadcrumb -->
+    <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#top-article"><h2 id="featured-listing">Rainbow Six (6) Mobile Spring Launch</h2></a>
+
+    <!-- Alpha Notes -->
+    <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-crossplay-controller-support"><h2>Rainbow Six (6) to feature Crossplay & Controller Support for Mobile</h2></a>
+    <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-update-alpha-test"><h2>Rainbow Six (6) Update Patch Notes Mobile - Alpha Test</h2></a>
+    <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-esports"><h2>4 Real Facts about - Rainbow Six (6) Mobile Esports Scene</h2></a>
+    <a href="/main-pages/rainbow-six-mobile/game-lounge-chatroom#exclusive-content"><h2>Rainbow Six (6) Mobile Exclusive Content</h2></a>
+    <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-operators"><h2>Rainbow Six (6) Mobile Upcoming Operators - Alpha Test</h2></a>
+    <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-release-date"><h2>Rainbow Six (6) Mobile Release Date</h2></a>
+
+    <!-- NOTES LINK -->
+    <a href="/main-pages/rainbow-six-mobile/soft-launch-notes/rainbow-six-mobile-soft-launch-notes"><h2>Rainbow Six Mobile Soft Launch Notes!</h2></a>
+
+    <!-- Statement Outreach -->
+    <a href="/main-pages/statement"><h2>Feenixbloom Gamelounge Statement Outreach</h2></a>
+`;
