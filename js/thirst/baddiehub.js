0796f6a65407c3bb0ecd154b470531a8e3ba7433
Onique-Unique <kiddohbeats@yahoo.com>
2022-07-05 08:28:09 -0400

    Content Update

---
+++ js/thirst/baddiehub.js
@@ -0,0 +1,198 @@
+var after = "";
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function fetchMemes() {
+  color();
+
+  if (document.getElementById("memes-container")) {
+    document.getElementById("memes-container").remove();
+  }
+  var newIndex = [];
+  var id = 0;
+  var lastIndex=[];
+    let parentdiv = document.createElement("div");
+    let secondarydiv = document.createElement("div");
+    let loadMoreBtn = document.createElement("button");
+    parentdiv.id = "memes-container";
+    secondarydiv.id = "memes";
+    loadMoreBtn.id = "load-more";
+  fetch(`https://www.reddit.com/r/IRLgirls.json?after=${after}`)
+    .then((response) => response.json())
+    .then((body) => {
+      after = body.data.after;
+      for (let index = 0; index < body.data.children.length; index++) {
+        if (body.data.children[index].data.post_hint === "image") {
+          id=index;
+          newIndex.push(id);
+          console.log(index);          
+        }
+      }
+      // console.log(newIndex);
+      shuffle(newIndex);
+      // console.log(newIndex);
+      return body;
+    }).then(body=>{
+      newIndex.forEach((elem)=>{
+              let div = document.createElement("div");
+              let h4 = document.createElement("h4");
+              let image = document.createElement("img");
+              image.src = body.data.children[elem].data.url_overridden_by_dest;
+              h4.textContent = body.data.children[elem].data.title;
+              loadMoreBtn.innerHTML = "Load More";
+              div.id = "memes-inner";
+              div.appendChild(h4);
+              div.appendChild(image);
+              secondarydiv.appendChild(div);
+              parentdiv.appendChild(secondarydiv);
+              parentdiv.appendChild(loadMoreBtn);
+              document.body.appendChild(parentdiv);
+            })
+            document.querySelector(".content-wrap").appendChild(parentdiv);
+            const memeBtn = document.getElementById("load-more");
+            memeBtn.addEventListener("click", () => {
+            fetchMemes();
+            window.scroll({top: 0, behavior: "smooth"});
+        })
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+const lowerMetaContent = document.querySelector(".lower-meta-content");
+window.onload = function () {
+    fetchMemes();
+
+    setInterval(function() { 
+        lowerMetaContent.classList.remove("hide");
+    }, 2000);
+};
+
+const shareBtn = document.getElementById("button");
+function shareList() {
+    if (navigator.canShare) {
+        navigator.share({
+            title: "BaddieHub Best Looking Girls",
+            text: "Hottest BaddiesHub Girls",
+            url: window.location.href,
+        });
+    } else {
+        // Desktop Functionality
+    }
+};
+
+shareBtn.addEventListener("click", () => {
+    shareList();
+});
+
+// mobile menu variables
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+
+  // mobile menu function
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove('active');
+    overlay.classList.remove('active');
+  }
+
+  mobileMenuOpenBtn[i].addEventListener('click', function () {
+    mobileMenu[i].classList.add('active');
+    overlay.classList.add('active');
+  });
+
+  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+  overlay.addEventListener('click', mobileMenuCloseFunc);
+}
+
+// Cookie Settings
+document.cookie = "AC-C=ac-c;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=None;Secure";
+
+// Contact Send to avoid spam and hide email from email crawlers
+const contactSend = document.getElementById("contact-submit");
+const action = "https://formsubmit.co/oniquecampbell@yahoo.com";
+
+contactSend.addEventListener("click", () => {
+    document.getElementById("gamelounge-contact").action = action;
+});
+
+
+// Contact Email & Privacy Policy
+const landingPageVisible = document.querySelector('.landing-page-visible');
+const contactForm = document.querySelector('.gamelounge-contact-container');
+const contactClick = document.querySelector(".showcase-contact");
+const disclaimerClick = document.querySelector(".showcase-disclaimer");
+const contactFormExit = document.querySelector('.form-exit');
+const disclaimerForm = document.querySelector('.fair-use-disclaimer--container');
+const disclaimerExit = document.querySelector('.disclaimer-exit');
+
+contactClick.addEventListener("click", () => {
+    landingPageVisible.classList.add("hide");
+    contactForm.classList.toggle("hide");
+});
+
+contactFormExit.addEventListener("click", () => {
+    landingPageVisible.classList.remove("hide");
+    contactForm.classList.toggle("hide");
+});
+
+disclaimerClick.addEventListener("click", () => {
+    landingPageVisible.classList.add("hide");
+    disclaimerForm.classList.toggle("hide");
+});
+
+disclaimerExit.addEventListener("click", () => {
+    landingPageVisible.classList.remove("hide");
+    disclaimerForm.classList.toggle("hide");
+});
+
+
+const detect = document.querySelector("#detect");
+wrapper = document.querySelector(".wrapper");
+button = wrapper.querySelector(".ad-ok-btn");
+
+// possible classes that adblocker avoid to render
+let adClasses = ["ad", "ads", "adsbox", "ad-placement", "doubleclick", "ad-placeholder", "ad-badge"];
+for(let item of adClasses) {
+    detect.classList.add(item); // adding all array item in detect element
+}
+
+let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
+
+button.addEventListener("click", () => {
+
+    if (landingPageVisible.classList.contains("hide")) {
+        landingPageVisible.classList.remove("hide");
+    };
+    wrapper.classList.remove("show"); // hide popup on button click
+    window.location.reload();
+});
+
+if(!wrapper.classList.contains("show")) {
+    // if display property value is none then show the popup else hide it
+getProperty == "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
+landingPageVisible.classList.add("hide");
+    if (getProperty == "block") {
+        landingPageVisible.classList.remove("hide");
+    };
+}
+console.log(getProperty);
