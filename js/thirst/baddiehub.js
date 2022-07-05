543c727418d78d0f56bdefadbd153ce9a71eaa88
Onique-Unique <kiddohbeats@yahoo.com>
2022-07-05 10:40:52 -0400

    Content Update

--- js/thirst/baddiehub.js
+++ js/thirst/baddiehub.js
@@ -165,34 +165,35 @@ disclaimerExit.addEventListener("click", () => {
     disclaimerForm.classList.toggle("hide");
 });
 
+if(window.canRunAds === undefined) {
+    const detect = document.querySelector("#detect");
+    wrapper = document.querySelector(".wrapper");
+    button = wrapper.querySelector(".ad-ok-btn");
 
-const detect = document.querySelector("#detect");
-wrapper = document.querySelector(".wrapper");
-button = wrapper.querySelector(".ad-ok-btn");
+    // possible classes that adblocker avoid to render
+    let adClasses = ["ad", "ads", "adsbox", "ad-placement", "doubleclick", "ad-placeholder", "ad-badge"];
+    for(let item of adClasses) {
+        detect.classList.add(item); // adding all array item in detect element
+    }
 
-// possible classes that adblocker avoid to render
-let adClasses = ["ad", "ads", "adsbox", "ad-placement", "doubleclick", "ad-placeholder", "ad-badge"];
-for(let item of adClasses) {
-    detect.classList.add(item); // adding all array item in detect element
+    let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
+
+    button.addEventListener("click", () => {
+
+        if (landingPageVisible.classList.contains("hide")) {
+            landingPageVisible.classList.remove("hide");
+        };
+        wrapper.classList.remove("show"); // hide popup on button click
+        window.location.reload();
+    });
+
+    if(!wrapper.classList.contains("show")) {
+        // if display property value is none then show the popup else hide it
+    getProperty == "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
+    landingPageVisible.classList.add("hide");
+        if (getProperty == "block") {
+            landingPageVisible.classList.remove("hide");
+        };
+    }
 }
 
-let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
-
-button.addEventListener("click", () => {
-
-    if (landingPageVisible.classList.contains("hide")) {
-        landingPageVisible.classList.remove("hide");
-    };
-    wrapper.classList.remove("show"); // hide popup on button click
-    window.location.reload();
-});
-
-if(!wrapper.classList.contains("show")) {
-    // if display property value is none then show the popup else hide it
-getProperty == "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
-landingPageVisible.classList.add("hide");
-    if (getProperty == "block") {
-        landingPageVisible.classList.remove("hide");
-    };
-}
-console.log(getProperty);
