2f6c6b817dd31c0152b0cb70ecc5da1d531e1a7a
Onique-Unique <kiddohbeats@yahoo.com>
2022-07-18 14:12:03 -0400

    Content Update

--- js/blog-pages.js/archive-modal.js
+++ js/blog-pages.js/archive-modal.js
@@ -2,13 +2,17 @@ document.getElementById("popular-posts-box").innerHTML =
 `
 <h3 class="title">popular posts</h3>
 <div class="p-post">
+<a href="/fee/crypto-fee-comparison">
+        <h3>01. Crypto Fee Comparison</h3>
+        <span><i class="far fa-clock"></i>&nbsp; <time datetime="2022-07-18">Jul 18, 2022 &bull; 9 min read</time></span>              
+    </a>
     <a href="/fee/binance-bitcoin-withdrawal-fee">
-        <h3>01. Binance Bitcoin Withdrawal Fee</h3>
+        <h3>02. Binance Bitcoin Withdrawal Fee</h3>
         <span><i class="far fa-clock"></i>&nbsp; <time datetime="2022-07-18">Jul 18, 2022 &bull; 6 min read</time></span>              
     </a>
     <a href="/fee/binance-eth-withdrawal-fee">
-        <h3>02. Binance Ethereum (ETH) Withdrawal Fee</h3>
-        <span><i class="far fa-clock"></i>&nbsp; <time datetime="2022-07-18">Jul 18, 2022 &bull; 6 min read</time></span>              
+        <h3>03. Binance Ethereum (ETH) Withdrawal Fee</h3>
+        <span><i class="far fa-clock"></i>&nbsp; <time datetime="2022-07-18">Jul 18, 2022 &bull; 7 min read</time></span>              
     </a>
 </div>
 `;
