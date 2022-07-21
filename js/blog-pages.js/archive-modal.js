ae76a04848d0521b5160a58e616302e0ffaad67e
Onique-Unique <kiddohbeats@yahoo.com>
2022-07-21 09:32:42 -0400

    Content Update

--- js/blog-pages.js/archive-modal.js
+++ js/blog-pages.js/archive-modal.js
@@ -18,5 +18,9 @@ document.getElementById("popular-posts-box").innerHTML =
         <h3>04. Webull Stock Transfer Fee</h3>
         <span><i class="far fa-clock"></i>&nbsp; <time datetime="2022-07-20">Jul 20, 2022 &bull; 6 min read</time></span>              
     </a>
+    <a href="/fee/low-transaction-fee-crypto">
+        <h3>05. Low Transaction Fee Crypto</h3>
+        <span><i class="far fa-clock"></i>&nbsp; <time datetime="2022-07-21">Jul 21, 2022 &bull; 9 min read</time></span>              
+    </a>
 </div>
 `;
