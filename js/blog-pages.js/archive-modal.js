99ca9b8f5161440f2ca99fd5b3763737217c0782
Onique-Unique <kiddohbeats@yahoo.com>
2022-07-22 08:43:55 -0400

    Content Update

--- js/blog-pages.js/archive-modal.js
+++ js/blog-pages.js/archive-modal.js
@@ -1,3 +1,62 @@
+// Related Links
+window.onload = function () {
+    // Array of links and anchor link name to be related by category by order
+    var relatedLinks = ["/fee/binance-eth-withdrawal-fee", "/fee/binance-bitcoin-withdrawal-fee", "/fee/crypto-fee-comparison" , "/fee/low-transaction-fee-crypto" , "/fee/webull-stock-transfer-fee"];
+    var aLinkName = ["Binance ETH Withdrawal Fee", "Binance Bitcoin Withdrawal Fee", "Crypto Fee Comparison", "Low Transaction Fee Crypto", "Webull Stock Transfer Fee"];
+    
+    // store url pathname as string into current variable 
+    const currentUrl = window.location.pathname.toString();
+    var i;
+
+    // check if related links contain converted current Url string in array
+    if (relatedLinks.includes(currentUrl)) {
+
+        // if current Url exists in array - remove from link list & link name by it's corresponding index
+        const linkIndex = relatedLinks.indexOf(currentUrl);
+        relatedLinks.splice(linkIndex,1);
+        aLinkName.splice(linkIndex, 1);
+
+        // store new array in new variables
+        var linkItem = relatedLinks;
+        var nameItem = aLinkName;
+
+        // execute a for loop for new set of array - get elements by their new index
+        for (i = 0; i < linkItem.length && nameItem.length; i++) {
+            idLink = linkItem[i];
+            idText = nameItem[i];
+
+            // create new element list item - insert innerhtml components
+            let linkList = document.createElement("li");
+            linkList.innerHTML = 
+            `
+            <a href="${idLink}">${idText}</a>
+            `;
+
+            // find element by it's id inside document and append created new list item
+            document.getElementById("related-link-list").appendChild(linkList);
+        }
+    } else {
+        // execute a for loop for new set of array - get elements by their new index
+        for (i = 0; i < relatedLinks.length && aLinkName.length; i++) {
+            idLink = relatedLinks[i];
+            idText = aLinkName[i];
+
+            // create new element list item - insert innerhtml components
+            let linkList = document.createElement("li");
+            linkList.innerHTML = 
+            `
+            <a href="${idLink}">${idText}</a>
+            `;
+
+            // find element by it's id inside document and append created new list item
+            document.getElementById("related-link-list").appendChild(linkList);
+        }
+    }
+};
+
+
+
+// Sidebar Table Links
 document.getElementById("popular-posts-box").innerHTML = 
 `
 <h3 class="title">popular posts</h3>
