db8ab2e799c699cadccda388acf920273a05641f
Onique-Unique <kiddohbeats@yahoo.com>
2022-10-21 18:24:00 -0400

    Content Update

--- feenixbloom-template-editor/htmlCode.js
+++ feenixbloom-template-editor/htmlCode.js
@@ -31,8 +31,8 @@ let htmlCode_first = `<!DOCTYPE html>
                     "@type": "SpeakableSpecification",
                     "xpath": ["/html/head/title", "/html/head/meta[@name='description']/@content"]
                 },
-                "datePublished": "2022-10-17",
-                "dateModified": "2022-10-17",
+                "datePublished": "2022-10-22",
+                "dateModified": "2022-10-22",
                 "author": [{
                     "@type": "Person",
                     "name": "Onique Campbell",
@@ -198,7 +198,7 @@ let htmlCode_first = `<!DOCTYPE html>
                     <div class="h1-blog">
                         <h1 class="title"><span>Here it is:</span><b id="share-title"> { .title } </b><span class="yearInsert"></span>! </h1>
                         <div class="date">
-                            <i class="far fa-clock"></i><a href="#">&nbsp; Onique Campbell</a> &bull; <span><time datetime="2022-10-17">Oct 17, 2022</time></span>
+                            <i class="far fa-clock"></i><a href="#">&nbsp; Onique Campbell</a> &bull; <span><time datetime="2022-10-22">Oct 22, 2022</time></span>
                         </div>
                         <div class="breadcrumbs-container" itemscope itemtype="https://schema.org/BreadcrumbList">
                             <ul class="breadcrumbs breadcrumbs-alt" aria-label="Breadcrumbs">
