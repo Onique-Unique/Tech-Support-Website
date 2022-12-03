9a925affc1ec0728ee729bf86d79b76793bc5caf
Onique-Unique <kiddohbeats@yahoo.com>
2022-12-03 10:31:11 -0500

    Content Update

--- js/pokemon.js
+++ js/pokemon.js
@@ -142,6 +142,136 @@ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
 // Cookie Settings
 document.cookie = "AC-C=ac-c;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=None;Secure";
 
+// Coupon Select/ Display for Popup
+let springCoupons = [ { code: "SUMMERFUN20", discount: "20% off" }, { code: "NEWYEAR30", discount: "30% off" }, { code: "SUPERSALE20", discount: "20% off" }, { code: "SUMMERFUN30", discount: "30% off" }, { code: "SPRINGFREE50", discount: "Free shipping over $50" }, { code: "SPRING20OFF", discount: "20% off" }, { code: "MARCH20OFF", discount: "20% off" }, { code: "SPRINGSALE10", discount: "10% off" }, { code: "APRIL15OFF", discount: "15% off" }, { code: "SPRING50OFF", discount: "50% off" } ];
+
+let summerCoupons = [ { code: "SALE25OFF", discount: "25% off" }, { code: "FREESHIPPING50", discount: "Free shipping over $50" }, { code: "SUPERSALE20", discount: "20% off" }, { code: "FREESHIPJUNE50", discount: "Free shipping over $50" }, { code: "SUMMER20OFF", discount: "20% off" }, { code: "JULY10OFF", discount: "10% off" }, { code: "JULY30OFF", discount: "30% off" }, { code: "AUGUST15OFF", discount: "15% off" }, { code: "SUMMER50OFF", discount: "50% off" }, { code: "SUMMERSHIP20", discount: "Free shipping over $20" } ];
+
+let fallCoupons = [ { code: "BLACKFRIDAY50", discount: "50% off" }, { code: "CYBERMONDAY30", discount: "30% off" }, { code: "LABORDAY25", discount: "25% off" }, { code: "OCTOBERSALE20", discount: "20% off" }, { code: "FALL20OFF", discount: "20% off" }, { code: "SEPT10OFF", discount: "10% off" }, { code: "OCTOBER30OFF", discount: "30% off" }, { code: "NOVEMBER15OFF", discount: "15% off" }, { code: "FALL50OFF", discount: "50% off" }, { code: "FALLSHIP20", discount: "Free shipping over $20" } ];
+
+let winterCoupons = [ { code: "HALLOWEEN30", discount: "30% off" }, { code: "FREESHIPPINGNOV50", discount: "Free shipping over $50" }, { code: "THANKSGIVING30", discount: "30% off" }, { code: "BLACKFRIDAY50", discount: "50% off" }, { code: "CYBERMONDAY35", discount: "35% off" }, { code: "CHRISTMAS20", discount: "20% off" }, { code: "WINTER20OFF", discount: "20% off" }, { code: "DECEMBER10OFF", discount: "10% off" }, { code: "JANUARY30OFF", discount: "30% off" }, { code: "FEBRUARY15OFF", discount: "15% off" }, { code: "WINTER50OFF", discount: "50% off" }, { code: "WINTERSHIP20", discount: "Free shipping over $20" } ];
+
+// add function that checks current date, if the date is season relative then select a random coupon code from that list
+
+function getCouponCodeBySeason() {
+  let currentDate = new Date();
+  let month = currentDate.getMonth();
+  let coupons;
+
+  if (month >= 3 && month <= 5) {
+    coupons = springCoupons;
+  } else if (month >= 6 && month <= 8) {
+    coupons = summerCoupons;
+  } else if (month >= 9 && month <= 11) {
+    coupons = fallCoupons;
+  } else if (month === 0 || month === 1 || month === 2 || month === 12) {
+    coupons = winterCoupons;
+  }
+
+  // exclude all codes that have a month name that is not equal to current month
+  let filteredCoupons = coupons.filter(coupon => {
+    let code = coupon.code;
+    let monthName = code.substring(0, 3).toUpperCase();
+    let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    let currentMonth = months[month];
+    return monthName !== currentMonth;
+  });
+
+  let randomIndex = Math.floor(Math.random() * filteredCoupons.length);
+  let selectedCoupon = filteredCoupons[randomIndex];
+  return selectedCoupon;
+}
+
+let couponCode = getCouponCodeBySeason();
+console.log(couponCode);
+
+// Onpage Popup 
+const userSearch = document.querySelector(".content-hub-heading b").innerText;
+let popup = document.createElement('div');
+popup.className = 'popup';
+
+let popupContent = document.createElement('div');
+popupContent.className = 'popup-content';
+
+let popupInnerContent = document.createElement('div');
+popupInnerContent.className = 'popup-inner-content';
+
+let h3 = document.createElement('h3');
+let h3Text = document.createTextNode('Hurry!!');
+h3.appendChild(h3Text);
+
+let h4Main = document.createElement('h4');
+h4Main.id = "popup-main-h4";
+h4Main.innerHTML = `Interested In ${userSearch}?`;
+
+let h4Secondary = document.createElement('h4');
+h4Secondary.innerHTML = `<span id="span-head">Amazon</span> is showcasing special deals right now on the products you want to buy`;
+
+let p = document.createElement('p');
+let pText = document.createTextNode('Go quick and save your items to cart to lock in these deals for the holidays..');
+p.appendChild(pText);
+
+let pCoupon = document.createElement('p');
+pCoupon.className = "coupon-text";
+pCoupon.innerHTML = `Missed Your Chance To Use Coupon? Try One Now: <br><br><span id="coupon-code">${couponCode.code}</span>`;
+
+let pCouponText = document.createElement('p');
+pCouponText.className = "coupon-text";
+pCouponText.innerHTML = `${couponCode.discount}`;
+
+let popupURLBtn = document.createElement('button');
+popupURLBtn.className = "popup-btn";
+let btnText = document.createTextNode('Take Me There');
+popupURLBtn.appendChild(btnText);
+popupURLBtn.onclick = gotoURL;
+
+let exitPopup = document.createElement("div");
+exitPopup.className = "exit-popup";
+exitPopup.innerHTML = `<i class="fa-regular fa-circle-xmark"></i>`;
+popupContent.appendChild(exitPopup);
+exitPopup.onclick = closePopup;
+
+popupInnerContent.appendChild(h3);
+popupInnerContent.appendChild(h4Main);
+popupInnerContent.appendChild(h4Secondary);
+popupInnerContent.appendChild(p);
+popupInnerContent.appendChild(pCoupon);
+popupInnerContent.appendChild(pCouponText);
+popupInnerContent.appendChild(popupURLBtn);
+
+popupContent.appendChild(popupInnerContent);
+popup.appendChild(popupContent);
+
+document.body.appendChild(popup);
+let showPopup = true;
+
+function closePopup() {
+  document.getElementsByClassName('popup')[0].style.display = 'none';
+  showPopup = false;
+  document.querySelector(".landing-page-visible").classList.remove("popup-blur");
+  document.body.classList.remove("page-body-popup-adjust");
+}
+
+function gotoURL() {
+  document.location.href = 'https://www.amazon.com?&linkCode=ll2&tag=oniquecampbel-20&linkId=9e1e91f2e3d89d93e1684445475d82ee&language=en_US&ref_=as_li_ss_tl';
+}
+
+document.onscroll = function () {
+  if (window.scrollY > (window.innerHeight * 2) && showPopup) {
+    document.getElementsByClassName('popup')[0].style.display = 'block';
+    document.querySelector(".landing-page-visible").classList.add("popup-blur");
+    document.body.classList.add("page-body-popup-adjust");
+  }
+};
+
+let timer = setInterval(function () {
+  if (showPopup == false) {
+    showPopup = true;
+    document.getElementsByClassName('popup')[0].style.display = 'block';
+    document.body.classList.add("page-body-popup-adjust");
+  }
+}, 25000);
+
 // Contact Send to avoid spam and hide email from email crawlers
 const contactSend = document.getElementById("contact-submit");
 const action = "https://formsubmit.co/oniquecampbell@yahoo.com";
