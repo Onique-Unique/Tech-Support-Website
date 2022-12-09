6f3177dcc5f10815f93a6e4d865e486f911be6e7
Onique-Unique <kiddohbeats@yahoo.com>
2022-12-09 10:11:45 -0500

    Content Update

---
+++ js/youth-size-pages.js
@@ -0,0 +1,384 @@
+// For Youth Size Pages Only - insert onto page
+if(document.getElementById('youth-size-table')) {
+    document.getElementById('youth-size-table').innerHTML = `
+    <br>
+    <h2></h2>
+      <div class="table-container youth-table-wrap">
+      <p class="text">Pants:</p>
+          <table>
+              <thead>
+                  <tr>
+                      <th>US</th>
+                      <th class="mobile-remove">Canada</th>
+                      <th class="mobile-remove">EU</th>
+                      <th>Measurements</th>
+                  </tr>
+                  <tr>
+                      <th></th>
+                      <th class="mobile-remove"></th>
+                      <th class="mobile-remove"></th>
+                      <th>Waist, Length</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>XS (2-4)</td>
+                      <td class="mobile-remove">XS (2-4)</td>
+                      <td class="mobile-remove">XS (128)</td>
+                      <td>
+                          <p class="status status-low">20”, 19”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>S (6-8)</td>
+                      <td class="mobile-remove">S (6-8)</td>
+                      <td class="mobile-remove">S (140)</td>
+                      <td>
+                          <p class="status status-low">22”, 20”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>M (10-12)</td>
+                      <td class="mobile-remove">M (10-12)</td>
+                      <td class="mobile-remove">M (152)</td>
+                      <td>
+                          <p class="status status-low">24”, 22”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>L (14-16)</td>
+                      <td class="mobile-remove">L (14-16)</td>
+                      <td class="mobile-remove">L (164)</td>
+                      <td>
+                          <p class="status status-low">26”, 24”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>XL (18-20)</td>
+                      <td class="mobile-remove">XL (18-20)</td>
+                      <td class="mobile-remove">XL (176)</td>
+                      <td>
+                          <p class="status status-mid">28”, 26”</p>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+      <br>
+      <h2></h2>
+      <div class="table-container youth-table-wrap">
+      <p class="text">Shorts:</p>
+          <table>
+              <thead>
+                  <tr>
+                      <th>US</th>
+                      <th class="mobile-remove">Canada</th>
+                      <th class="mobile-remove">EU</th>
+                      <th>Measurements</th>
+                  </tr>
+                  <tr>
+                      <th></th>
+                      <th class="mobile-remove"></th>
+                      <th class="mobile-remove"></th>
+                      <th>Waist, Length</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>XS (2-4)</td>
+                      <td class="mobile-remove">XS (2-4)</td>
+                      <td class="mobile-remove">XS (128)</td>
+                      <td>
+                          <p class="status status-low">20”, 11”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>S (6-8)</td>
+                      <td class="mobile-remove">S (6-8)</td>
+                      <td class="mobile-remove">S (140)</td>
+                      <td>
+                          <p class="status status-low">22”, 12”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>M (10-12)</td>
+                      <td class="mobile-remove">M (10-12)</td>
+                      <td class="mobile-remove">M (152)</td>
+                      <td>
+                          <p class="status status-low">24”, 13”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>L (14-16)</td>
+                      <td class="mobile-remove">L (14-16)</td>
+                      <td class="mobile-remove">L (164)</td>
+                      <td>
+                          <p class="status status-low">26”, 14”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>XL (18-20)</td>
+                      <td class="mobile-remove">XL (18-20)</td>
+                      <td class="mobile-remove">XL (176)</td>
+                      <td>
+                          <p class="status status-mid">28”, 15”</p>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+      <br>
+      <h2></h2>
+      <div class="table-container youth-table-wrap">
+      <p class="text">Shirt:</p>
+          <table>
+              <thead>
+                  <tr>
+                      <th>US</th>
+                      <th class="mobile-remove">Canada</th>
+                      <th class="mobile-remove">EU</th>
+                      <th>Measurements</th>
+                  </tr>
+                  <tr>
+                      <th></th>
+                      <th class="mobile-remove"></th>
+                      <th class="mobile-remove"></th>
+                      <th>Chest, Length</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>XS (2-4)</td>
+                      <td class="mobile-remove">XS (2-4)</td>
+                      <td class="mobile-remove">XS (128)</td>
+                      <td>
+                          <p class="status status-low">25”, 21”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>S (6-8)</td>
+                      <td class="mobile-remove">S (6-8)</td>
+                      <td class="mobile-remove">S (140)</td>
+                      <td>
+                          <p class="status status-low">27”, 22</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>M (10-12)</td>
+                      <td class="mobile-remove">M (10-12)</td>
+                      <td class="mobile-remove">M (152)</td>
+                      <td>
+                          <p class="status status-low">29”, 23”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>L (14-16)</td>
+                      <td class="mobile-remove">L (14-16)</td>
+                      <td class="mobile-remove">L (164)</td>
+                      <td>
+                          <p class="status status-low">31”, 24”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>XL (18-20)</td>
+                      <td class="mobile-remove">XL (18-20)</td>
+                      <td class="mobile-remove">XL (176)</td>
+                      <td>
+                          <p class="status status-mid">33”, 25”</p>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+      <br>
+      <h2></h2>
+      <div class="table-container youth-table-wrap">
+      <p class="text">Shoes:</p>
+          <table>
+              <thead>
+                  <tr>
+                      <th>US</th>
+                      <th class="mobile-remove">Canada</th>
+                      <th class="mobile-remove">EU</th>
+                      <th>Measurements</th>
+                  </tr>
+                  <tr>
+                      <th></th>
+                      <th class="mobile-remove"></th>
+                      <th class="mobile-remove"></th>
+                      <th>Length, Width</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>XS (2-4)</td>
+                      <td class="mobile-remove">XS (2-4)</td>
+                      <td class="mobile-remove">XS (128)</td>
+                      <td>
+                          <p class="status status-low">7.25”, 4.25”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>S (6-8)</td>
+                      <td class="mobile-remove">S (6-8)</td>
+                      <td class="mobile-remove">S (140)</td>
+                      <td>
+                          <p class="status status-low">8.25”, 4.75”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>M (10-12)</td>
+                      <td class="mobile-remove">M (10-12)</td>
+                      <td class="mobile-remove">M (152)</td>
+                      <td>
+                          <p class="status status-low">9.25”, 5.25”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>L (14-16)</td>
+                      <td class="mobile-remove">L (14-16)</td>
+                      <td class="mobile-remove">L (164)</td>
+                      <td>
+                          <p class="status status-low">10.25”, 5.75”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>XL (18-20)</td>
+                      <td class="mobile-remove">XL (18-20)</td>
+                      <td class="mobile-remove">XL (176)</td>
+                      <td>
+                          <p class="status status-mid">11.25”, 6.25”</p>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+      <br>
+      <h2></h2>
+      <div class="table-container youth-table-wrap">
+      <p class="text">Gildan:</p>
+          <table>
+              <thead>
+                  <tr>
+                      <th>US</th>
+                      <th class="mobile-remove">Canada</th>
+                      <th class="mobile-remove">EU</th>
+                      <th>Measurements</th>
+                  </tr>
+                  <tr>
+                      <th></th>
+                      <th class="mobile-remove"></th>
+                      <th class="mobile-remove"></th>
+                      <th>Chest, Length</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>XS (2-4)</td>
+                      <td class="mobile-remove">XS (2-4)</td>
+                      <td class="mobile-remove">XS (128)</td>
+                      <td>
+                          <p class="status status-low">25”, 21”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>S (6-8)</td>
+                      <td class="mobile-remove">S (6-8)</td>
+                      <td class="mobile-remove">S (140)</td>
+                      <td>
+                          <p class="status status-low">27”, 22”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>M (10-12)</td>
+                      <td class="mobile-remove">M (10-12)</td>
+                      <td class="mobile-remove">M (152)</td>
+                      <td>
+                          <p class="status status-low">29”, 23”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>L (14-16)</td>
+                      <td class="mobile-remove">L (14-16)</td>
+                      <td class="mobile-remove">L (164)</td>
+                      <td>
+                          <p class="status status-low">31”, 24”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>XL (18-20)</td>
+                      <td class="mobile-remove">XL (18-20)</td>
+                      <td class="mobile-remove">XL (176)</td>
+                      <td>
+                          <p class="status status-mid">33”, 25”</p>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+      <br>
+      <h2></h2>
+      <div class="table-container youth-table-wrap">
+      <p class="text">Jacket:</p>
+          <table>
+              <thead>
+                  <tr>
+                      <th>US</th>
+                      <th class="mobile-remove">Canada</th>
+                      <th class="mobile-remove">EU</th>
+                      <th>Measurements</th>
+                  </tr>
+                  <tr>
+                      <th></th>
+                      <th class="mobile-remove"></th>
+                      <th class="mobile-remove"></th>
+                      <th>Chest, Length</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>XS (2-4)</td>
+                      <td class="mobile-remove">XS (2-4)</td>
+                      <td class="mobile-remove">XS (128)</td>
+                      <td>
+                          <p class="status status-low">25”, 21”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>S (6-8)</td>
+                      <td class="mobile-remove">S (6-8)</td>
+                      <td class="mobile-remove">S (140)</td>
+                      <td>
+                          <p class="status status-low">27”, 22”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>M (10-12)</td>
+                      <td class="mobile-remove">M (10-12)</td>
+                      <td class="mobile-remove">M (152)</td>
+                      <td>
+                          <p class="status status-low">29”, 23”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>L (14-16)</td>
+                      <td class="mobile-remove">L (14-16)</td>
+                      <td class="mobile-remove">L (164)</td>
+                      <td>
+                          <p class="status status-low">31”, 24”</p>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>XL (18-20)</td>
+                      <td class="mobile-remove">XL (18-20)</td>
+                      <td class="mobile-remove">XL (176)</td>
+                      <td>
+                          <p class="status status-mid">33”, 25”</p>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+  
+    `;
+  }
