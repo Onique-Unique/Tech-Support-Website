bc5f1912fd8783e2adf54e5ee364d55e8480ab96
Onique-Unique <kiddohbeats@yahoo.com>
2022-12-17 11:19:07 -0500

    Content Update

--- js/sitemap-generator.js
+++ js/sitemap-generator.js
@@ -1,10 +1,11 @@
 var domain = window.location.hostname;
 var links = document.querySelectorAll('a');
+var OgDomain = "www.feenixbloom.com";
 var domainXML = `
 <url>
-<loc>https://${domain}/</loc>
+<loc>https://${OgDomain}/</loc>
 <lastmod>${new Date().toISOString()}</lastmod>
-<priority>1.0</priority>
+<priority>1.00</priority>
 </url>`;
 var sitemapGen = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" \n xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \n xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 \n http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n ${domainXML} \n`;
 var linkHrefs = [];
@@ -12,8 +13,14 @@ var linkHrefs = [];
 for (let link of links) {
 if (link.hostname == domain && !link.href.includes('#')) {
     var linkHref = link.href.split('.html')[0];
+    // Change hostname to OgDomain
+    linkHref = linkHref.replace(domain, OgDomain);
+    // Remove next 5 characters after www.feenixbloom.com/ OgDomain
+    linkHref = linkHref.replace(/www.feenixbloom.com(.{5})/, OgDomain);
+    // Replace any instance of http with https
+    linkHref = linkHref.replace(/http:/g, 'https:');
     if (!linkHrefs.includes(linkHref)) {
-    sitemapGen += '<url>\n<loc>' + linkHref + '</loc>\n<lastmod>' + new Date().toISOString() + '</lastmod>\n<priority>1.0</priority>\n</url>\n';
+    sitemapGen += '<url>\n<loc>' + linkHref + '</loc>\n<lastmod>' + new Date().toISOString() + '</lastmod>\n<priority>1.00</priority>\n</url>\n';
     linkHrefs.push(linkHref);
     }
 }
@@ -24,9 +31,9 @@ sitemapGen += '</urlset>';
 // Store sitemap data in local storage
 localStorage.setItem('sitemapGen', sitemapGen);
 
-// Create a link for the sitemap
-var linkElement = document.createElement('a');
-linkElement.style.color = "aliceblue";
-linkElement.href = `https://${domain}/view-sitemap.html`;
-linkElement.innerText = 'View Sitemap';
-document.body.appendChild(linkElement);
+// Create a link for the sitemap - Uncomment If Needed
+// var linkElement = document.createElement('a');
+// linkElement.style.color = "aliceblue";
+// linkElement.href = `https://${domain}/view-sitemap.html`;
+// linkElement.innerText = 'View Sitemap';
+// document.body.appendChild(linkElement);
