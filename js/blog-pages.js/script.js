10ed3ba18e1e39cb404ed7ddf66187aa0d927563
Onique-Unique <kiddohbeats@yahoo.com>
2023-01-29 06:11:04 -0500

    Content Update

--- js/blog-pages.js/script.js
+++ js/blog-pages.js/script.js
@@ -129,7 +129,7 @@ if (shareBtn) {
  // Create modal title element
  var roadmapModalTitle = document.createElement("h3");
  roadmapModalTitle.classList.add("modal-title");
- roadmapModalTitle.innerHTML = "Change Your <br> Life";
+ roadmapModalTitle.innerHTML = "Are We <br> Hiring?";
  roadmapModalContent.appendChild(roadmapModalTitle);
 
  // Create modal subtitle element
@@ -151,7 +151,7 @@ if (shareBtn) {
  // Add modal content to modal element
  roadmapModal.appendChild(roadmapModalContent);
 
- var roadmapNodes = ["main-post-2", "review-2", "review-5", "review-7", "review-9", "review-12", "review-15", "review-19", "review-21", "review-24"];
+ var roadmapNodes = ["main-post-2", "review-2", "review-4", "review-6", "review-8", "review-11", "review-14", "review-16", "review-19", "review-21", "review-24"];
  for (var i = 0; i < roadmapNodes.length; i++) {
    var roadmapNode = document.getElementById(roadmapNodes[i]);
    if (roadmapNode) {
