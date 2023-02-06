a913326445eac38ec100d50130879f3d64c819fe
Onique-Unique <kiddohbeats@yahoo.com>
2023-02-06 10:35:23 -0500

    Content Update

--- passive-earning-main/landing-page/assets/js/script.js
+++ passive-earning-main/landing-page/assets/js/script.js
@@ -21,7 +21,7 @@ document.querySelectorAll(".passive-contact").forEach(el => {
         <input name="email" placeholder="Your Email Address..." type="email" tabindex="2" required>
     </fieldset>
     <fieldset>
-        <button name="submit" type="submit" class="contact-submit" data-submit="...Sending">Submit</button>
+        <button name="submit" type="submit" class="contact-submit" data-submit="...Sending">Get Roadmap Now</button>
     </fieldset>
   `;
 });
@@ -258,74 +258,75 @@ function addQuestion() {
 
 // Protect Page from unwanted content stealing, plagiarism etc such as copying content or inspect element...
 // Create error message box and design
-let errorMessage = document.createElement('div');
-errorMessage.className = 'error-message';
-errorMessage.style.background = 'ghostwhite';
-errorMessage.style.color = 'red';
-errorMessage.style.padding = '10px';
-errorMessage.style.position = 'fixed';
-errorMessage.style.top = '50%';
-errorMessage.style.left = '50%';
-errorMessage.style.transform = 'translate(-50%, -50%)';
-errorMessage.style.borderRadius = '5px';
-errorMessage.style.zIndex = '2';
-errorMessage.style.fontSize = '1.2rem';
-errorMessage.style.display = 'none';
-errorMessage.style.textAlign = "center";
-errorMessage.style.minWidth = "280px";
-errorMessage.innerText = 'Not Allowed: This page is protected!';
-document.body.style.userSelect = "none";
-document.body.style.WebkitUserSelect = "none";
-document.body.style.msUserSelect = "none";
-document.body.style.oUserSelect = "none";
-document.body.appendChild(errorMessage);
 
-// Detect attempts to copy any text on page
-document.addEventListener('copy', event => {
-  // Block the copy event and display error message
-  errorMessage.style.display = 'block';
-  setTimeout(function () {
-    errorMessage.style.display = 'none';
-  }, 1000);
-  event.preventDefault();
-});
+// let errorMessage = document.createElement('div');
+// errorMessage.className = 'error-message';
+// errorMessage.style.background = 'ghostwhite';
+// errorMessage.style.color = 'red';
+// errorMessage.style.padding = '10px';
+// errorMessage.style.position = 'fixed';
+// errorMessage.style.top = '50%';
+// errorMessage.style.left = '50%';
+// errorMessage.style.transform = 'translate(-50%, -50%)';
+// errorMessage.style.borderRadius = '5px';
+// errorMessage.style.zIndex = '2';
+// errorMessage.style.fontSize = '1.2rem';
+// errorMessage.style.display = 'none';
+// errorMessage.style.textAlign = "center";
+// errorMessage.style.minWidth = "280px";
+// errorMessage.innerText = 'Not Allowed: This page is protected!';
+// document.body.style.userSelect = "none";
+// document.body.style.WebkitUserSelect = "none";
+// document.body.style.msUserSelect = "none";
+// document.body.style.oUserSelect = "none";
+// document.body.appendChild(errorMessage);
 
-// Listen for right click on page then display error message
-document.addEventListener("contextmenu", function (event) {
-  errorMessage.style.display = 'block';
-  setTimeout(function () {
-    errorMessage.style.display = 'none';
-  }, 1000);
-  event.preventDefault();
-});
+// // Detect attempts to copy any text on page
+// document.addEventListener('copy', event => {
+//   // Block the copy event and display error message
+//   errorMessage.style.display = 'block';
+//   setTimeout(function () {
+//     errorMessage.style.display = 'none';
+//   }, 1000);
+//   event.preventDefault();
+// });
 
-// Find all text elements on page excluding anchor tags, images etc, listen for mousedown/ long press then display error message
-let textElements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6');
+// // Listen for right click on page then display error message
+// document.addEventListener("contextmenu", function (event) {
+//   errorMessage.style.display = 'block';
+//   setTimeout(function () {
+//     errorMessage.style.display = 'none';
+//   }, 1000);
+//   event.preventDefault();
+// });
 
-for (let i = 0; i < textElements.length; i++) {
-  textElements[i].addEventListener("mousedown", function (event) {
-    if (event.target.tagName != "a") {
-      event.preventDefault();
-    }
-  });
-}
+// // Find all text elements on page excluding anchor tags, images etc, listen for mousedown/ long press then display error message
+// let textElements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6');
 
-// Listen for keyboard control A or control C or control U or control shift J or control shift I or F12 on page then display error message
-document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey && event.keyCode == 65 || event.ctrlKey && event.keyCode == 67 || event.ctrlKey && event.shiftKey && event.keyCode == 74 || event.ctrlKey && event.keyCode == 85 || event.keyCode == 123 || event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    errorMessage.style.display = 'block';
-    setTimeout(function () {
-      errorMessage.style.display = 'none';
-    }, 1000);
-    event.preventDefault();
-  }
-});
+// for (let i = 0; i < textElements.length; i++) {
+//   textElements[i].addEventListener("mousedown", function (event) {
+//     if (event.target.tagName != "a") {
+//       event.preventDefault();
+//     }
+//   });
+// }
 
-// Check every 1 second if body user select has been removed then run the following function:
-setInterval(function () {
-  if (document.body.style.userSelect !== "none") {
-    document.body.style.display = "none";
-    window.location.reload();
-    alert("Really?! - You are attempting to do something that is not allowed!");
-  }
-}, 1000);
+// // Listen for keyboard control A or control C or control U or control shift J or control shift I or F12 on page then display error message
+// document.addEventListener("keydown", function (event) {
+//   if (event.ctrlKey && event.keyCode == 65 || event.ctrlKey && event.keyCode == 67 || event.ctrlKey && event.shiftKey && event.keyCode == 74 || event.ctrlKey && event.keyCode == 85 || event.keyCode == 123 || event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+//     errorMessage.style.display = 'block';
+//     setTimeout(function () {
+//       errorMessage.style.display = 'none';
+//     }, 1000);
+//     event.preventDefault();
+//   }
+// });
+
+// // Check every 1 second if body user select has been removed then run the following function:
+// setInterval(function () {
+//   if (document.body.style.userSelect !== "none") {
+//     document.body.style.display = "none";
+//     window.location.reload();
+//     alert("Really?! - You are attempting to do something that is not allowed!");
+//   }
+// }, 1000);
