6ccd7d586076fe187d66bc056dd991e1b7f5ba36
Onique-Unique <kiddohbeats@yahoo.com>
2023-02-11 12:02:47 -0500

    Content Update

--- js/rss-generator.js
+++ js/rss-generator.js
@@ -13,11 +13,12 @@ var rssGen = `<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
 
     xmlns:georss="http://www.georss.org/georss"
     xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"
+    xmlns:media="http://search.yahoo.com/mrss/"
     >
-  <!--Current RSS Feed links: ${anchorsWithHostname.length + 1} -->
   <channel>
     <title>Feenixbloom Website</title>
     <atom:link href="https://${OgDomain}/feed" rel="self" type="application/rss+xml" />
+    <ttl>60</ttl>
     <link>https://${OgDomain}</link>
     <description>Feenixbloom - Entertainment, Investment | Finance, Informational Content</description>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
@@ -32,41 +33,38 @@ var rssGen = `<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
     </image>
 `;
 
-// Count all unique links/ anchor tags related to hostname
-var anchors = document.querySelectorAll('a[href]');
-var hostname = window.location.hostname;
-var anchorsWithHostname = [];
-
-for (var i = 0; i < anchors.length; i++) {
-    var anchor = anchors[i];
-    var link = anchor.href;
-    if (link.includes(hostname) && !link.includes('#')) {
-        if (anchorsWithHostname.indexOf(link) === -1) {
-            anchorsWithHostname.push(link);
-        }
-    }
-}
-
 // Create an empty set to store the unique links
 var uniqueLinks = new Set();
 
-for (let link of links) {
-    if (link.hostname == domain && !link.href.includes('#')) {
-      var linkHref = link.href.split('.html')[0];
-      // Change hostname to OgDomain
-      linkHref = linkHref.replace(domain, OgDomain);
-      // Remove next 5 characters after www.feenixbloom.com/ OgDomain
-      linkHref = linkHref.replace(/www.feenixbloom.com(.{5})/, OgDomain);
-      // Replace any instance of http with https
-      linkHref = linkHref.replace(/http:/g, 'https:');
-      // Check if the link has already been added to the set of unique links
-      if (!uniqueLinks.has(linkHref)) {
-        // If not, add it to the set and add an <item> to the RSS feed
-        uniqueLinks.add(linkHref);
-        rssGen += '<item>\n<title>' + link.textContent + '</title>\n<link>' + linkHref + '</link>\n<pubDate>' + new Date().toUTCString() + '</pubDate>\n<guid isPermaLink="false">' + link.textContent + '</guid></item>\n';
-      }
+// Count from backwards/ latest
+for (let i = links.length - 1; i >= 0; i--) {
+
+  // Find Up to the 10 latest content links and coresponding images belonging to the misc-guides div on the index page
+  if (uniqueLinks.size >= 10) {
+    break;
+  }
+  let link = links[i];
+  // Anchor links in misc-guides div 
+  if (link.hostname == domain && !link.href.includes('#') && link.closest("#misc-guides")) {
+    var linkHref = link.href.split('.html')[0];
+    // Change hostname to OgDomain
+    linkHref = linkHref.replace(domain, OgDomain);
+    // Remove next 5 characters after www.feenixbloom.com/ OgDomain
+    linkHref = linkHref.replace(/www.feenixbloom.com(.{5})/, OgDomain);
+    // Replace any instance of http with https
+    linkHref = linkHref.replace(/http:/g, 'https:');
+
+    // The innerText of the data-img sibling
+    var imageUrl = link.nextElementSibling.innerText;
+    
+    // Check if the link has already been added to the set of unique links
+    if (!uniqueLinks.has(linkHref)) {
+      // If not, add it to the set and add an <item> to the RSS feed
+      uniqueLinks.add(linkHref);
+      rssGen += '<item>\n<title>' + link.textContent + '</title>\n<link>' + linkHref + '</link>\n' + `<media:content url="https://www.feenixbloom.com/images/blog-pages/${imageUrl}" medium="image"/>\n` + '<pubDate>' + new Date().toUTCString() + '</pubDate>\n<guid isPermaLink="false">' + link.textContent + '</guid></item>\n';
     }
-  }  
+  }
+}
 
 // close the channel and rss
 rssGen += '  </channel>\n</rss>';
